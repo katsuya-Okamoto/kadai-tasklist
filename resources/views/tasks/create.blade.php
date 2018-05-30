cd9c4615d6ad78728049ab84bc31b71b13b4a79d
katsuya.Okamoto <sume425@eay.jp>
2018-05-30 14:33:12 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,8 +6,16 @@
     
     {!! Form::model($task, ['route'=>'tasks.store']) !!}
     
-        {!! Form::label('taskvalue', 'タスク') !!}
-        {!! Form::text('taskvalue') !!}
+        {!! Form::label('status', '状態:') !!}
+        {!! Form::select('status', [
+            '未'=>'未',
+            '着手'=>'着手',
+            'まだまだ'=>'まだまだ',
+            '済'=>'済',
+        ]) !!}
+
+        {!! Form::label('content', 'タスク') !!}
+        {!! Form::text('content') !!}
         
         {!! Form::submit('作成') !!}
     
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,7 +6,15 @@
     
     {!! Form::model($task, ['route'=>['tasks.update', $task->id], 'method'=>'put']) !!}
 
-        {!! Form::label('content', 'タスク') !!}
+        {!! Form::label('status', '状態:') !!}
+        {!! Form::select('status', [
+            '未'=>'未',
+            '着手'=>'着手',
+            'まだまだ'=>'まだまだ',
+            '済'=>'済',
+        ]) !!}
+
+       {!! Form::label('content', 'タスク') !!}
         {!! Form::text('content') !!}
         
         {!! Form::submit('更新') !!}
