cfa5e751860ef1a1dc90887f6f16ae07a88f1055
katsuya.Okamoto <sume425@eay.jp>
2018-06-07 01:37:44 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
+//タスク一覧
 Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
